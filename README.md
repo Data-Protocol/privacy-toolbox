9ea558da5c6bb3057c2439973abc6061f9fb1389
Geoff Hollingworth <eusholli@gmail.com>
2021-06-14 15:45:43 +0100

    update LICENSE to CC-BY-SA-4.0

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Privacy Checklists
 
+Shield: [![CC BY-SA 4.0][cc-by-sa-shield]][cc-by-sa]
+
+This work is licensed under a
+[Creative Commons Attribution-ShareAlike 4.0 International License][cc-by-sa].
+
+[![CC BY-SA 4.0][cc-by-sa-image]][cc-by-sa]
+
+[cc-by-sa]: http://creativecommons.org/licenses/by-sa/4.0/
+[cc-by-sa-image]: https://licensebuttons.net/l/by-sa/4.0/88x31.png
+[cc-by-sa-shield]: https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg
+
 Community driven best practice developer checklists to avoid making mistakes and being wrong.
 
 ![Privacy by Design applied image](./images/pbdapplied.png)
--- sdk/ghlc-process.js
+++ sdk/ghlc-process.js
@@ -1,61 +1,29 @@
 // index.js
 const Mustache = require('mustache');
 const fs = require('fs');
-const checklistData = require('./checklist-data.json');
 
-const LIFECYCLE = [
-  'general',
-  'collection',
-  'maintenence',
-  'storage',
-  'usage',
-  'publication',
-  'archival',
-  'erasure',
-];
+const DATA_DIR = './../checklists-data/';
+const REPORT_DIR = './../checklists/';
 
-function sortData(importData) {
-  let sortedData = {};
-
-  for (let i = 0; i < LIFECYCLE.length; i++) {
-    let stage = LIFECYCLE[i];
-    let stageData = importData[stage];
-    let idea = [];
-    let test = [];
-    let production = [];
-    for (let j = 0; j < stageData.length; j++) {
-      let entry = stageData[j];
-      switch (entry.when) {
-        case 'Idea':
-          idea.push(entry);
-          break;
-        case 'Test':
-          test.push(entry);
-          break;
-        case 'Production':
-          production.push(entry);
-          break;
-        default:
-          console.error('Unknown design state (when): ' + entry);
-      }
-    }
-    sortedData[stage] = {
-      idea,
-      test,
-      production,
-    };
+function generateReports(dataFiles) {
+  for (let i = 0; i < dataFiles.length; i++) {
+    let checklistTemplate = `./../templates/checklist.mustache`;
+    fs.readFile(checklistTemplate, (err, container) => {
+      if (err) throw err;
+      const output = Mustache.render(container.toString(), stageData);
+      fs.writeFileSync(`${LIFECYCLE[i]}.md`, output);
+    });
   }
-  return sortedData;
 }
 
-const sortedData = sortData(checklistData);
+fs.readdir(DATA_DIR, function (err, files) {
+  //handling error
+  if (err) {
+    console.error('Unable to scan directory: ' + err);
+  }
+  const jsonFiles = files
+    .filter((file) => file.endsWith('.json') && file !== 'example-checklist.json')
+    .files.map((file) => path.join(DATA_DIR, file));
 
-for (let i = 0; i < LIFECYCLE.length; i++) {
-  let template = `${LIFECYCLE[i]}.mustache`;
-  fs.readFile(template, (err, container) => {
-    if (err) throw err;
-    let stageData = { checklist: sortedData[LIFECYCLE[i]] };
-    const output = Mustache.render(container.toString(), stageData);
-    fs.writeFileSync(`${LIFECYCLE[i]}.md`, output);
-  });
-}
+  generateReports(jsonFiles);
+});
